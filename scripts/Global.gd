0410bacc2fb1461ecc3255153c9e69108bf5e6f9
3cxc <81449257+3cxc@users.noreply.github.com>
2024-12-07 19:04:50 +0800

    game scripts
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

---
+++ scripts/Global.gd
@@ -0,0 +1,14 @@
+extends Node
+
+#全局变量
+
+#Steam是否在线
+var is_online: bool = false
+#是否已经购买游戏
+var is_owned: bool = false
+#Steam用户ID
+var steam_id: int = 0
+#APP ID
+var app_id: int = 2919180
+#用户使用的语言
+var language: String = "schinese"
---
+++ scripts/texts/ui/menu/buttons/load.gd
@@ -0,0 +1,4 @@
+extends Label
+
+func _ready() -> void:
+	set_text(Language.button_main_load)
